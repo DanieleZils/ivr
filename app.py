e2f0c8d7cafcc4dfb34de6de1e3ea40277f6634d
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-08-10 16:32:09 -0400

    cleanup

--- app.py
+++ app.py
@@ -19,39 +19,36 @@ client = signalwire_client(account_sid, auth_token, signalwire_space_url='daniel
 def home():
     return "Daniele's IVR"
 
-# Route to handle incoming calls
+# incoming calls
 @app.route('/incoming_call', methods=['POST'])
 def incoming_call():
     response = VoiceResponse()
 
-    # Create a Gather to handle user input
+   
     with Gather(numDigits=1, action='/handle-key', method='POST') as gather:
         gather.say("Thanks for calling Daniele. Press 1 for support, Press 2 for sales.")
 
-    # If no input received, redirect back to the main menu
+  
     response.append(gather) 
 
     return str(response)
 
-# Route to handle user input
+
 @app.route('/handle-key', methods=['POST'])
 def handle_key():
     digit_pressed = request.form['Digits']
 
     if digit_pressed == '1':
-        # Handle support option (you can add your logic here)
         response = VoiceResponse()
         response.say("You selected support. Forwarding your call to the support team.")
        
 
     elif digit_pressed == '2':
-        # Handle sales option (you can add your logic here)
         response = VoiceResponse()
         response.say("You selected sales. Forwarding your call to the sales team.")
        
 
     else:
-        # Invalid option
         response = VoiceResponse()
         response.say("Invalid option. Please try again.")
         response.redirect('/incoming_call')
